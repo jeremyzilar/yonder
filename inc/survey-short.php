44c0757ab4024d4edb666011abcd04710e8b2ffd
Jeremy Zilar <jeremyzilar@gmail.com>
2014-11-14 21:59:16 -0500

    cleaning up files

--- inc/survey-short.php
+++
@@ -1,7 +0,0 @@
-<section id="survey-short">
-  <div class="container">
-    <div class="row">        
-    	<h3><a href="#">Take our 2-minute survey <i class="fa fa-arrow-circle-right"></i></a></h3>
-    </div>
-  </div>
-</section>
