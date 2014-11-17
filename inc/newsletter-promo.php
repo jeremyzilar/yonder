171c6d69a0fe029c7443d96bb6d2d6645b564286
Jeremy Zilar <jeremyzilar@gmail.com>
2014-11-17 17:13:16 -0500

    new mail chimp signup

--- inc/newsletter-promo.php
+++ inc/newsletter-promo.php
@@ -8,7 +8,7 @@
 					<div class="mailing-list-box">
 						<!-- Begin MailChimp Signup Form -->
 						<div id="mc_embed_signup">
-							<form action="//foodcoop.us8.list-manage.com/subscribe/post?u=db95c2efca0aa6dbb61511337&amp;id=7e1e347b3d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate="" role="form">
+							<form action="//yondernews.us9.list-manage.com/subscribe/post?u=6f5470c92d20b5e4713750f26&amp;id=d1345c5295" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate="" role="form">
 								<div class="form-group mc-field-group">
 									<label for="mce-EMAIL">Email Address </label>
 									<input type="email" value="" name="EMAIL" class="form-control input-lg required email" id="mce-EMAIL" placeholder="E-mail Address">
@@ -20,7 +20,7 @@
 								</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 
 								<div style="position: absolute; left: -5000px;">
-									<input type="text" name="b_db95c2efca0aa6dbb61511337_7e1e347b3d" tabindex="-1" value="">
+									<input type="text" name="b_6f5470c92d20b5e4713750f26_d1345c5295" tabindex="-1" value="">
 								</div>
 							</form>
 						</div>
