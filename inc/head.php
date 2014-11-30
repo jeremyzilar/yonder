11eba8911436b17b0e35221e570ffa9b73839c60
Jeremy Zilar <jeremyzilar@gmail.com>
2014-11-29 22:45:53 -0500

    subtitle changes

--- inc/head.php
+++ inc/head.php
@@ -6,7 +6,9 @@
       <div class="col-xs-12">
       	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/globe4.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a>
         <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></h1>
-        <h3>A weekly newsletter on the news from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>, <span>deivered every Sunday.</span></h3>
+        <h3>A weekly newsletter on the news from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>, <span>delivered every Sunday.</span></h3>
+        <h3 class="hidden">A review of the week's news, <span>from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>.</span></h3>
+        <h3 class="hidden">The the week's news from here to there, <span>from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>.</span></h3>
       </div>
     </div>
 
