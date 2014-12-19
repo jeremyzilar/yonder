eea526a39e14366817136310e68e07a30f8f2655
Jeremy Zilar <jeremyzilar@gmail.com>
2014-12-19 16:22:14 -0500

    fixes

--- inc/mission-box.php
+++ inc/mission-box.php
@@ -1,22 +1,20 @@
-<?php if (empty(yonder_getcookie())) { ?>
-	<section id="mission-box">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-5 mission-photo">
-					<img src="<?php echo THEME . '/img/andrej_1000.png'; ?>" alt="" class="img-responsive"/>
-				</div>
-				<div class="col-xs-12 col-sm-7 mission-txt">
-					<p>Yonder is a weekly newsletter on the news from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>, <span>deivered every Sunday.</span></p>
-					<p>Questions? <a href="mailto:andrej.mrevlje@gmail.com">andrej.mrevlje@gmail.com</a></p>
-
-					<!-- Social Promo -->
-    			<?php include(INC . '/social.php'); ?>
-
-				</div>
+<section id="mission-box">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-5 mission-photo">
+				<img src="<?php echo THEME . '/img/andrej_1000.png'; ?>" alt="" class="img-responsive"/>
 			</div>
-			
-			
+			<div class="col-xs-12 col-sm-7 mission-txt">
+				<p>Yonder is a weekly newsletter on the news from <a href="https://twitter.com/andrejmrevlje" title="Follow Andrej Mrevlje on Twitter">Andrej Mrevlje</a>, <span>deivered every Sunday.</span></p>
+				<p>Questions? <a href="mailto:andrej.mrevlje@gmail.com">andrej.mrevlje@gmail.com</a></p>
 
+				<!-- Social Promo -->
+  			<?php include(INC . '/social.php'); ?>
+
+			</div>
 		</div>
-	</section>
-<?php } ?>
+		
+		
+
+	</div>
+</section>
--- inc/newsletter-promo.php
+++ inc/newsletter-promo.php
@@ -1,28 +1,26 @@
-<?php if (empty(yonder_getcookie())) { ?>
-	<div class="row" id="newsletter-promo">
-		<div class="col-xs-12 box">
-			<h3>Sign-up for the Newsletter <i class="fa fa-envelope"></i></h3>
-			<div class="mailing-list-box">
-				<!-- Begin MailChimp Signup Form -->
-				<div id="mc_embed_signup">
-					<form action="//yondernews.us9.list-manage.com/subscribe/post?u=6f5470c92d20b5e4713750f26&amp;id=d1345c5295" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate="" role="form">
-						<div class="form-group mc-field-group">
-							<label for="mce-EMAIL">Email Address </label>
-							<input type="email" value="" name="EMAIL" class="form-control input-md required email" id="mce-EMAIL" placeholder="E-mail Address">
-						</div>
-						<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-md btn-submit">
-						<div id="mce-responses" class="clear hidden">
-							<div class="response" id="mce-error-response" style="display:none"></div>
-							<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+<div class="row" id="newsletter-promo">
+	<div class="col-xs-12 box">
+		<h3>Sign-up for the Newsletter <i class="fa fa-envelope"></i></h3>
+		<div class="mailing-list-box">
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="//yondernews.us9.list-manage.com/subscribe/post?u=6f5470c92d20b5e4713750f26&amp;id=d1345c5295" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate="" role="form">
+					<div class="form-group mc-field-group">
+						<label for="mce-EMAIL">Email Address </label>
+						<input type="email" value="" name="EMAIL" class="form-control input-md required email" id="mce-EMAIL" placeholder="E-mail Address">
+					</div>
+					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-md btn-submit">
+					<div id="mce-responses" class="clear hidden">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 
-						<div style="position: absolute; left: -5000px;">
-							<input type="text" name="b_6f5470c92d20b5e4713750f26_d1345c5295" tabindex="-1" value="">
-						</div>
-					</form>
-				</div>
-				<!--End mc_embed_signup-->
+					<div style="position: absolute; left: -5000px;">
+						<input type="text" name="b_6f5470c92d20b5e4713750f26_d1345c5295" tabindex="-1" value="">
+					</div>
+				</form>
 			</div>
+			<!--End mc_embed_signup-->
 		</div>
 	</div>
-<?php } ?>
+</div>
