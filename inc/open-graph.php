a5cb25241879521413a9d7852a5c1bddef3f58af
Jeremy Zilar <jeremyzilar@gmail.com>
2015-01-26 11:12:25 -0500

    Deleting images

--- inc/open-graph.php
+++ inc/open-graph.php
@@ -4,8 +4,9 @@
 <meta property="og:description" content="<?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" />
 <meta property="og:url" content="<?php echo esc_attr( get_bloginfo( 'url' ) ); ?>/" />
 <meta property="og:site_name" content="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+<meta property="og:image" content="<?php echo THEME . '/img/yonder-logo.png'; ?>" />
 <meta property="og:image" content="<?php echo THEME . '/img/andrej_1000.png'; ?>" />
-<meta property="og:image" content="<?php echo THEME . '/img/andrej.png'; ?>" />
+
 
 <!-- Twitter -->
 <link rel="me" href="https://twitter.com/andrejmrevlje" />
@@ -14,4 +15,4 @@
 <meta name="twitter:creator" content="@andrejmrevlje">
 <meta name="twitter:title" content="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 <meta name="twitter:description" content="<?php echo esc_attr( get_bloginfo( 'description' ) ); ?>">
-<meta name="twitter:image" content="<?php echo THEME . '/img/andrej_1000.png'; ?>" />
+<meta name="twitter:image" content="<?php echo THEME . '/img/yonder-logo.png'; ?>" />
