f35a8845d4c7ee95965b2ec8a6e431a362282ff5
Jeremy Zilar <jeremyzilar@gmail.com>
2017-01-05 11:36:06 -0500

    New logo and header style has been configured

--- inc/head.php
+++ inc/head.php
@@ -5,15 +5,9 @@
       <div class="col-xs-12">
       	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/assets/img/globe4.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a>
         <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></h1>
-        <h3>Connecting global events into a new narrative. <span>By <a href="<?php echo esc_url( home_url( '/' ) ); ?>about/" title="More about Andrej Mrevlje and Yonder">Andrej Mrevlje</a>.</span></h3>
+        <h3>by Andrej Mrevlje</h3>
       </div>
     </div>
-
-    <!-- Newsletter Promo -->
-    <?php include(INC . '/newsletter-promo.php'); ?>
-
-    <!-- Social Promo -->
-    <?php include(INC . '/social.php'); ?>
 
   </div>
 
