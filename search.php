437431705e11be81f363e7d0377be61878a683ca
Jeremy Zilar <jeremyzilar@gmail.com>
2017-02-25 18:25:33 -0500

    another id for search

--- search.php
+++ search.php
@@ -22,7 +22,7 @@ get_header(); ?>
     <section id="main">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-sm-7">
+          <div id="blog" class="col-xs-12 col-sm-7">
             <?php loop(); ?>
           </div>
           <div class="col-xs-12 col-sm-3">
